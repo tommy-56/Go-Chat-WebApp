b82717c81c3f75257acd136ec4eb34ffbd73f6fe
TommyA56Code <tommyandersonpersonal56@gmail.com>
2024-01-02 18:44:00 -0600

    Move Chat components to container
    Easier to work with to add new features later

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header';
-import ChatHistory from './components/ChatHistory/ChatHistory';
-import ChatInput from './components/ChatInput/ChatInput';
+import ChatContainer from './components/ChatContainer';
 import './App.css';
-import { connect, sendMsg } from './api';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chatHistory: []
-    }
-  }
-
-  componentDidMount() {
-    connect((msg) => {
-      console.log("New Message")
-      this.setState(prevState => ({
-        chatHistory: [...this.state.chatHistory, msg]
-      }))
-      console.log(this.state);
-    });
-  }
-  
-  send(event) {
-    if(event.keyCode === 13) {
-      sendMsg(event.target.value);
-      event.target.value = "";
-    }
-  }
+ 
 
   render() {
     return (
       <div className="App">
         <Header />
-        <ChatHistory chatHistory={this.state.chatHistory} />
-        <ChatInput send={this.send} />
+        <ChatContainer/>
       </div>
     );
   }
